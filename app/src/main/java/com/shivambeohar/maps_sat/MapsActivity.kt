9dd338fa65a19c0d9a4a18b16bd481070fae68fb
Shivam <beoharshivam@gmail.com>
2022-03-13 20:22:04 +0530

    Added method to Mark the locations on Map.

--- app/src/main/java/com/shivambeohar/maps_sat/MapsActivity.kt
+++ app/src/main/java/com/shivambeohar/maps_sat/MapsActivity.kt
@@ -7,9 +7,9 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.shivambeohar.maps_sat.databinding.ActivityMapsBinding
+import com.shivambeohar.maps_sat.models.MapLocation
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -41,8 +41,23 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val listOfPlaces: List<MapLocation> = listOf(
+            MapLocation.KOCHIN,
+            MapLocation.COIMBATORE,
+            MapLocation.MADURAI,
+            MapLocation.MUNNAR
+        )
+        addMarkerToPlaces(listOfPlaces)
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(MapLocation.KOCHIN.coordinate))
+    }
+
+    /**
+     * Add Marker on the Places provided in the List.
+     * @param listOfPlaces Places that needs to be marked.
+     */
+    private fun addMarkerToPlaces(listOfPlaces: List<MapLocation>) {
+        for (location in listOfPlaces) {
+            mMap.addMarker(MarkerOptions().position(location.coordinate).title(location.name))
+        }
     }
 }
